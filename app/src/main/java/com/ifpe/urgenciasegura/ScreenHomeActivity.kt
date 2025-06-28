0a824078c9186692ee6219ce81dc11c509c4c8b7
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-06-28 02:39:28 -0300

    Updating activity_screen_home.xml and ScreenHomeActivity.kt

--- app/src/main/java/com/ifpe/urgenciasegura/ScreenHomeActivity.kt
+++ app/src/main/java/com/ifpe/urgenciasegura/ScreenHomeActivity.kt
@@ -2,10 +2,14 @@ package com.ifpe.urgenciasegura
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.ifpe.urgenciasegura.ui.minhasurgencias.MinhasUrgenciasActivity
+import com.ifpe.urgenciasegura.ui.minhasurgencias.MinhasUrgenciasFragment
 
 class ScreenHomeActivity : AppCompatActivity() {
 
@@ -16,32 +20,37 @@ class ScreenHomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_home)
 
+        val btnMinhasUrgencias = findViewById<Button>(R.id.buttonMinhasUrgencias)
         val prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE)
         val nomeUsuario = prefs.getString("nome", "Usuário")
 
-        // Atualiza a mensagem de boas-vindas
         val welcomeMessage = findViewById<TextView>(R.id.welcomeMessage)
         welcomeMessage.text = "Olá, $nomeUsuario! Tudo pronto para te ajudar."
 
-        // Referências aos botões
         buttonSolicitarUrgencia = findViewById(R.id.buttonSolicitarUrgencia)
         buttonLogout = findViewById(R.id.buttonLogout)
 
-        // Ação do botão Solicitar Urgência
         buttonSolicitarUrgencia.setOnClickListener {
             val intent = Intent(this, RequestUrgencyActivity::class.java)
             startActivity(intent)
         }
-
-        // Ação do botão Logout
+        btnMinhasUrgencias.setOnClickListener {
+            val intent = Intent(this, MinhasUrgenciasActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+        }
         buttonLogout.setOnClickListener {
             prefs.edit().clear().apply()
             FirebaseAuth.getInstance().signOut()
-
             val intent = Intent(this, ScreenLoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
             finish()
         }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
+        return true
     }
 }
