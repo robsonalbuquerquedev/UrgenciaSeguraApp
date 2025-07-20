732e33ce52bf2f2a7289d2203ee12d2c376d248d
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-07-20 17:59:17 -0300

    chore: add SonarQube plugin and config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+tasks.named("sonar").configure {
+    doFirst {
+        project.extensions.extraProperties["sonar.projectKey"] = project.property("sonar.projectKey")
+        project.extensions.extraProperties["sonar.host.url"] = project.property("sonar.host.url")
+        project.extensions.extraProperties["sonar.token"] = project.property("sonar.token")
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,7 @@
 pluginManagement {
+    plugins {
+        id("org.sonarqube") version "4.4.1.3373"
+    }
     repositories {
         google {
             content {
