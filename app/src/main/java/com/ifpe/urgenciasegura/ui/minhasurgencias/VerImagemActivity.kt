dd088083167973d2bfa3325bad21730d3a3ec7b6
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-07-29 20:02:50 -0300

    update: applying viewbinding

--- app/src/main/java/com/ifpe/urgenciasegura/ui/minhasurgencias/VerImagemActivity.kt
+++ app/src/main/java/com/ifpe/urgenciasegura/ui/minhasurgencias/VerImagemActivity.kt
@@ -1,23 +1,24 @@
 package com.ifpe.urgenciasegura.ui.minhasurgencias
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.ifpe.urgenciasegura.R
+import com.ifpe.urgenciasegura.databinding.ActivityVerImagemBinding
 import com.squareup.picasso.Picasso
 
 class VerImagemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVerImagemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ver_imagem)
+        binding = ActivityVerImagemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val imageView = findViewById<ImageView>(R.id.imageViewFull)
         val url = intent.getStringExtra("fotoUrl")
 
         Picasso.get()
             .load(url)
             .placeholder(android.R.drawable.ic_menu_gallery)
             .error(android.R.drawable.ic_dialog_alert)
-            .into(imageView)
+            .into(binding.imageViewFull)
     }
 }
